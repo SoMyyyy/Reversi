66bad5446cb3847ea8c38bc51ac3fddc04911463
SoMyyyy <90850135+SoMyyyy@users.noreply.github.com>
2024-05-15 15:16:24 +0200

    Delete ReversiMVCApplication directory

--- ReversiMVCApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReversiMVCApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ReversiMVCApplication/Models/UserRoleViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReversiMVCApplication.Models;
-
-public class UserRoleViewModel
-{
-    public string UserId { get; set; }
-    public string UserName { get; set; }
-    public IList<string> Roles { get; set; }
-    public IList<string> AllRoles { get; set; }
-}
